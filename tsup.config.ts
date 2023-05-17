c59ed20ef14c2fd3eb916100a360c821cfa9c4bc
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-05-17 17:15:54 +0530

    disable css check

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "tsup";
 import cssModulesPlugin from "esbuild-css-modules-plugin";
-import { sassPlugin } from "esbuild-sass-plugin";
 
 export default defineConfig({
-  esbuildPlugins: [cssModulesPlugin(), sassPlugin()],
+  esbuildPlugins: [cssModulesPlugin()],
   splitting: true,
   minify: true,
   format: ["cjs", "esm"],
